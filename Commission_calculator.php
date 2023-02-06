d8f5fd56ef5b24fd6c6be4d0cf3837880fd70b96
Liton Miah <123876372+litonmiah13@users.noreply.github.com>
2023-02-07 01:21:08 +0600

    Update Commission_calculator.php

--- Commission_calculator.php
+++ Commission_calculator.php
@@ -6,10 +6,8 @@ $tuitionFee = readline('Enter the value '); /* We want to take the input from ke
 $commission1 = $tuitionFee*.25; //Commission is 25% when it's above $20000
 $commission2 = $tuitionFee*.20; //Commission is 20% when it's above $10000 but less than $20000
 $commission3 = $tuitionFee*.15; //Commission is 15% when it's above $7000 but less than $10000
-$a=15;
 
 $commission = $tuitionFee >= 20000 ? "Commission is {$commission1}" : ($tuitionFee>=10000 && $tuitionFee<20000 ? "Commission is {$commission2}" : ($tuitionFee >= 7000 && $tuitionFee < 10000? "Commission is {$commission3}" : "Invalid"));
-
 
 echo $commission;
 
